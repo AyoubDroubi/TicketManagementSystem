99fa35253195f6eabcd046ba267015412ddb0318
Ayoub Droubi <ayoub.al.droubi@gmail.com>
2026-08-13 21:46:32 +0300

    Remove legacy hardcoded default password

--- Infrastructure/Common/Constants.cs
+++ Infrastructure/Common/Constants.cs
@@ -2,12 +2,9 @@
 {
     public class Constants
     {
-        public const string DEFAULT_PASSWORD = "NeedReset%123";
-
         public const string STATUS_OPEN = "OPEN";
         public const string STATUS_NEW = "NEW";
         public const string STATUS_CLOSED = "CLOSED";
-
 
         public const string ROLE_ADMIN_ID = "48395b9b-a292-4a48-80df-ebc6a057f6c0";
         public const string ROLE_USER_ID = "6367a40d-3014-43bc-a704-135f41abae2d";
@@ -19,7 +16,6 @@
             { ROLE_ADMIN_ID, ROLE_ADMIN },
             { ROLE_USER_ID, ROLE_USER }
         };
-
 
         public const string DEFAULT_AVATAR = "avatar.png";
     }
