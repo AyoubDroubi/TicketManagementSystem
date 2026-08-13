374788f8ec111aee2f25f96e9100f76bf0473a6d
Ayoub Droubi <ayoub.al.droubi@gmail.com>
2026-08-13 21:27:58 +0300

    Wire V2 backend layers

--- src/Ticketing.Api/Program.cs
+++ src/Ticketing.Api/Program.cs
@@ -1,4 +1,6 @@
+using Ticketing.Application;
 using Ticketing.Application.Abstractions;
+using Ticketing.Infrastructure;
 using Ticketing.Infrastructure.Time;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddProblemDetails();
 builder.Services.AddHealthChecks();
 builder.Services.AddSingleton<IClock, SystemClock>();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
